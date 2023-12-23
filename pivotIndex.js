c4bbeceddd26c1cf5d2a04b52abcf687f933b912
hodjik <154238856+hodjik@users.noreply.github.com>
2023-12-23 22:39:27 +0600

    четвертая задача

---
+++ pivotIndex.js
@@ -0,0 +1,18 @@
+function pivotIndex(nums) {
+    let totalNums = nums.reduce((res, num) => res + num)
+    let leftNum = 0
+        for (let i = 0; i < nums.length; i++) {
+            totalNums -= nums[i]
+
+                if (leftNum === totalNums){
+                    return i
+                }
+                
+                leftNum += nums[i]
+            
+        }
+
+        return -1
+
+}
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]))
